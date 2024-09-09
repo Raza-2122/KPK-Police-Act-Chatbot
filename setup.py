5851ac8a165ad4c59d5290bc634529ff3f72aac5
Syed Qasim Raza <84401201+Raza-2122@users.noreply.github.com>
2024-09-08 23:58:17 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kpk-police-act-chatbot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        'streamlit',
+        'other_dependencies',
+    ],
+)
